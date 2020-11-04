eed37cb3607a0f8e20e98c763269bf3e4eb3036d
JT-92 <57745836+JT-92@users.noreply.github.com>
2020-11-04 23:38:16 +0100

    avancement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmailService } from './email.service';
+ 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'listeBlancheFrontend';
+  emails = [];
+  
+  constructor(private emailService: EmailService) { }
+  
+  ngOnInit(){this.emailService.getAllEmails().subscribe(data => {this.emails = data}, error => {this.emails = []});}
 }
---
+++ src/app/email.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Email {
+	id: number
+	fk_user: number
+	email_sender: string
+	email_subject: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+
+  constructor(private http: HttpClient) { }
+  
+  getAllEmails(): Observable<Email[]> {
+    return this.http.get<Email[]>('http://a10dc2bd2b94.ngrok.io/api/emails')
+  }
+  
+  
+  
+}
