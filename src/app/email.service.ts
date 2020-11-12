c8388667b3612d753a03122d349810e3ef36cc9b
JT-92 <57745836+JT-92@users.noreply.github.com>
2020-11-12 12:54:51 +0100

    demo

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -18,11 +18,11 @@ export class EmailService {
   constructor(private http: HttpClient) { }
   
   getAllEmails(): Observable<Email[]> {
-<<<<<<< Updated upstream
-    return this.http.get<Email[]>('http://localhost:8000/api/emails')
-=======
+//<<<<<<< Updated upstream
+    //return this.http.get<Email[]>('http://localhost:8000/api/emails')
+//=======
     return this.http.get<Email[]>('http://23d807666b40.ngrok.io/api/emails')
->>>>>>> Stashed changes
+//>>>>>>> Stashed changes
   }
   
   
