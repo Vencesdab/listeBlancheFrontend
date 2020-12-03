e2d30db24d8d45496f8ab795c4d0b23ccbc0cbc2
agustin <aguszorza@gmail.com>
2020-12-03 01:22:32 +0100

    captcha page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,18 @@ import { MailComponent } from './modules/general/mail/mail.component';
 import { SingleMailComponent } from './modules/general/single-mail/single-mail.component';
 import { LoginComponent } from './modules/general/login/login.component';
 import { CreateAccountComponent } from './modules/general/create-account/create-account.component';
+import { CaptchaComponent } from './modules/general/captcha/captcha.component';
 
 const routes: Routes = [
-	{ path: '', component: LoginComponent,},
-	{ path: 'home', component: HomeComponent,},
-	{ path: 'profil', component: ProfilComponent,},
-	{ path: 'mail', component: MailComponent,},
-	{ path: 'single-mail', component: SingleMailComponent,},
-	{ path: 'login', component: LoginComponent,},
-	{ path: 'create-account', component: CreateAccountComponent,}
-	];
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'profil', component: ProfilComponent },
+  { path: 'mail', component: MailComponent },
+  { path: 'single-mail', component: SingleMailComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'create-account', component: CreateAccountComponent },
+  { path: 'verify/:id', component: CaptchaComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
