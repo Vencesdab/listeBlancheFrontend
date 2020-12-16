8a4f3e8d8315259789aed6b9eaf178e18eb2b8b7
JT-92 <57745836+JT-92@users.noreply.github.com>
2020-12-16 18:40:32 +0100

    transition apres creation d'user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import config  from './../config.json';
+import { Storable, StorageService } from './storage.service';
 
 
 
@@ -9,11 +10,13 @@ import config  from './../config.json';
 })
 export class UserService {
   adresse = config.url + "register"
+  
 
-constructor(private http: HttpClient) { }
+constructor(private http: HttpClient,  private storageService: StorageService) { }
 
 create(userdata) {
-  const headers = { 'content-type': 'application/json'}  
+  const headers = { 'content-type': 'application/json'}
+  this.storageService.store(Storable.haveAccount, true);  
   return this.http.post(this.adresse, userdata, {'headers':headers, withCredentials: !config["proxy-dev"]});
   }
 
